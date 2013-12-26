1a70dd49acb26caf491053d305cc38d4c9e7baab
hakimu jackson <hakimuj@gmail.com>
2013-12-25 21:14:30 -0800

    Update post_controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,18 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		@post = Post.new(params[:post])
+		@post.save
+		redirect_to @post
 	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+
 end
